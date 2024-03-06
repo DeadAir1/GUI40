04c9f146bb4bc091989d4a15de2cc7a97b0553a0
oleks <s27565@pjwstk.edu.pl>
2024-03-06 11:13:03 +0100

    GUI1

--- src/GUI1/ProstokatTest.java
+++ src/GUI1/ProstokatTest.java
@@ -9,7 +9,7 @@ public class ProstokatTest {
                 new ZProstokat(4, 'a', 'e'),        // konstruktor z 3 parametrami
                 new ZProstokat(5, 3, '*', '+'),    // konstruktor z 4 parametrami
                 new ZProstokat(1, 2, 'a', 'a'),
-                new ZProstokat(3, 3, '+', 'x'),
+                new ZProstokat(3, 3, 'x', '+'),
                 new ZProstokat(1, 2, 'x', 'y'),
                 new ZProstokat(3, 4, '^', '$')
         };
