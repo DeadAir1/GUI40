ebd0caf3a0b108147c9e558f9e3005421aaf1f46
oleks <s27565@pjwstk.edu.pl>
2024-04-23 21:27:20 +0200

    GUI

--- src/GUI_7/Reading.java
+++ src/GUI_7/Reading.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Reading {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException {
+        //Zadanie A
+        /*try {
             Stream<String> lines= Files.lines(Paths.get("students.txt"), StandardCharsets.UTF_8);
                 lines.filter(s-> Integer.parseInt(Arrays.stream(s.split(" +"))
                         .sorted(
@@ -19,17 +22,29 @@ public class Reading {
                           )
                         .toList()
                         .get(0))>50)
+                        .sorted(Comparator.reverseOrder())
                         .forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }*/
+        //Zadanie C
+        try {
+            Stream<String> lines= Files.lines(Paths.get("students.txt"), StandardCharsets.UTF_8);
+            lines.filter(s-> Integer.parseInt(Arrays.stream(s.split(" +"))
+                            .sorted(
+                                    Comparator.comparing(String::length)
+                            )
+                            .toList()
+                            .getFirst())>50)
+                    .sorted(Comparator.reverseOrder())
+                    .map(s-> s.split(" +"))
+                    .collect(Collectors.toMap(arr-> Arrays.stream(arr).filter(s->s.matches("\\d{2}\\w")).findFirst(),
+                            arr->Arrays.asList(Arrays.stream(arr).filter(s-> s.matches("\\w+")), Arrays.stream(arr).filter(s->s.matches("\\d{2}")))))
+                    .forEach((key,value)-> System.out.println(key+ " " + value));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
-//new Comparator<String>() {
-//                            @Override
-//                            public int compare(String o1, String o2) {
-//                                if(o1.length()>o2.length()) return 1;
-//                                if(o1.length()==o2.length()) return 0;
-//                                return -1;
-//                            }
-//                        }
